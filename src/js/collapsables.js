1ec68be91c3641db1e27109952c0a039f10fa358
Leire Ordeñana <leireomadina@gmail.com>
2020-10-15 13:56:59 +0200

    Add conditionals and icons

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -4,21 +4,28 @@
 const designContainer = document.querySelector(".js-design");
 const fillContainer = document.querySelector(".js-fill");
 const shareContainer = document.querySelector(".js-share");
-const formContainer = document.querySelectorAll(".js-");
+
+// const formContainer = document.querySelectorAll(".js-form");
 
 const designCollapsable = document.querySelector(".js-design-collapsable");
 const fillCollapsable = document.querySelector(".js-fill-collapsable");
 const shareCollapsable = document.querySelector(".js-share-collapsable");
 
-// function openCollapsable() {
-//   return designContainer.classList.toggle("hidden");
-// }
+function openCollapsableDesign() {
+  return designContainer.classList.toggle("hidden");
+}
 
-// function openCollapsable(event) {
-//   const selectedCollapsable = event.currentTarget;
-//   selectedCollapsable.classList.toggle("hidden");
-// }
+function openCollapsableFill() {
+  if (designContainer.classList.contains("hidden")) {
+    fillContainer.classList.remove("hidden");
+  }
+  return fillContainer.classList.toggle("hidden");
+}
 
-designCollapsable.addEventListener("click", openCollapsable);
-fillCollapsable.addEventListener("click", openCollapsable);
-shareCollapsable.addEventListener("click", openCollapsable);
+function openCollapsableShare() {
+  return shareContainer.classList.toggle("hidden");
+}
+
+designCollapsable.addEventListener("click", openCollapsableDesign);
+fillCollapsable.addEventListener("click", openCollapsableFill);
+shareCollapsable.addEventListener("click", openCollapsableShare);
