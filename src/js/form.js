b05bcc1ae03f516a8a81263f218aef8264b1125e
crisgarm <crisgarm9@gmail.com>
2020-10-16 14:37:07 +0200

    Add changes in object sintaxis

--- src/js/form.js
+++ src/js/form.js
@@ -3,27 +3,27 @@
 const inputList = document.querySelectorAll('.js-input');
 
 let data = {
-  name = "Nombre Apellido",
-  job = "Front-end developer",
-  email = "",
-  tel = "",
-  linkedin = "",
-  github ="",
-}
+  name: 'Nombre Apellido',
+  job: 'Front-end developer',
+  email: '',
+  tel: '',
+  linkedin: '',
+  github: '',
+};
 
 const saveField = function(event){
   event.preventDefault();
   console.log(event.currentTarget.id);
   console.log(event.currentTarget.value);
   data[event.currentTarget.id] = event.currentTarget.value;
-  render();
-}
+  // render();
+};
 
 for (const eachElement of inputList){
   eachElement.addEventListener('blur', saveField);
 }
 
 
-const render = function(){
-
-}
+// const render = function(){
+//
+// }
