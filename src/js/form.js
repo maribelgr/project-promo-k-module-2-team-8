ea4178262d04c3bd112fc717cb839ceb6792fd57
crisgarm <crisgarm9@gmail.com>
2020-10-16 21:39:08 +0200

    Add js form

--- src/js/form.js
+++ src/js/form.js
@@ -1,36 +1,42 @@
-"use strict";
+'use strict';
 
-const inputList = document.querySelectorAll(".js-input");
+const inputList = document.querySelectorAll('.js-input');
 
 let data = {
-  name: "Nombre Apellido",
-  job: "Front-end developer",
-  tel: "",
-  email: "",
-  linkedin: "",
-  github: "",
+  name: 'Nombre Apellido',
+  job: '',
+  tel: '',
+  email: '',
+  linkedin: '',
+  github: '',
 };
 
 const saveField = function (event) {
   event.preventDefault();
-  console.log(event.currentTarget.id);
-  console.log(event.currentTarget.value);
   data[event.currentTarget.id] = event.currentTarget.value;
   render();
 };
 
 for (const eachElement of inputList) {
-  eachElement.addEventListener("keyup", saveField);
+  eachElement.addEventListener('keyup', saveField);
 }
 
 const render = function () {
-  debugger;
-  const cardName = document.querySelector(".js-name");
-  cardName.innerHTML = data[event.currentTarget.id];
-  "" === data[event.currentTarget.id] &&
-    (cardName.innerHTML = "Nombre Apellido");
-  const cardJob = document.querySelector(".js-job");
-  cardJob.innerHTML = data[event.currentTarget.id];
-  "" === data[event.currentTarget.id] &&
-    (cardJob.innerHTML = "Front-end developer");
+  const cardName = document.querySelector('.js-name');
+  cardName.innerHTML = data.name;
+  '' === data.name && (cardName.innerHTML = 'Nombre Apellido'); //Cuando esté vacío, que me pinte el valor 'Nombre Apellido'
+  const cardJob = document.querySelector('.js-job');
+  cardJob.innerHTML = data.job;
+  '' === data.job && (cardJob.innerHTML = 'Front-end developer');
+  const cardTel = document.querySelector('.js-tel');
+  cardTel.href = data.tel;
+  const cardEmail = document.querySelector('.js-email');
+  cardEmail.href = 'mailto:' + data.email;
+  const cardLinkedin = document.querySelector('.js-linkedin');
+  cardLinkedin.href = 'https://www.linkedin.com/in/' + data.linkedin;
+  const cardGithub = document.querySelector('.js-github');
+  cardGithub.href = 'https://github.com/' + data.github;
 };
+
+//RESET
+// data.name = '';
