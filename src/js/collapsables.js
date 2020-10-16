d713213a0def2891f40b6bd56af31360cf00a06e
Leire Ordeñana <leireomadina@gmail.com>
2020-10-16 13:19:24 +0200

    Add arrow up

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -4,28 +4,34 @@
 const designContainer = document.querySelector(".js-design");
 const fillContainer = document.querySelector(".js-fill");
 const shareContainer = document.querySelector(".js-share");
-// const formContainer = document.querySelectorAll(".js-form");
 
 const designCollapsable = document.querySelector(".js-design-collapsable");
 const fillCollapsable = document.querySelector(".js-fill-collapsable");
 const shareCollapsable = document.querySelector(".js-share-collapsable");
 
+const designArrow = document.querySelector(".js-arrow-design");
+const fillArrow = document.querySelector(".js-arrow-fill");
+const shareArrow = document.querySelector(".js-arrow-share");
+
 function openCollapsableDesign() {
   designContainer.classList.toggle("hidden");
   fillContainer.classList.add("hidden");
   shareContainer.classList.add("hidden");
+  designArrow.classList.toggle("arrow-up");
 }
 
 function openCollapsableFill() {
   fillContainer.classList.toggle("hidden");
   designContainer.classList.add("hidden");
   shareContainer.classList.add("hidden");
+  fillArrow.classList.toggle("arrow-up");
 }
 
 function openCollapsableShare() {
   shareContainer.classList.toggle("hidden");
   designContainer.classList.add("hidden");
   fillContainer.classList.add("hidden");
+  shareArrow.classList.toggle("arrow-up");
 }
 
 designCollapsable.addEventListener("click", openCollapsableDesign);
