4becc3df05d706ff04a5d937bdf1c5efcd0e3fa4
crisgarm <crisgarm9@gmail.com>
2020-10-17 19:24:51 +0200

    Add changes in reset button

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 "use strict";
 
 const inputList = document.querySelectorAll(".js-input");
@@ -9,6 +10,18 @@ const cardTel = document.querySelector(".js-tel");
 const cardEmail = document.querySelector(".js-email");
 const cardLinkedin = document.querySelector(".js-linkedin");
 const cardGithub = document.querySelector(".js-github");
+
+const colorName = document.querySelector(".js-name");
+const colorJob = document.querySelector(".js-job");
+const colorBorder = document.querySelector(".js-border");
+const colorPhone = document.querySelector(".js-color-tel");
+const colorEmail = document.querySelector(".js-color-email");
+const colorLinkedin = document.querySelector(".js-color-linkedin");
+const colorGithub = document.querySelector(".js-color-github");
+const colorBorderPhone = document.querySelector(".js-border-tel");
+const colorBorderEmail = document.querySelector(".js-border-email");
+const colorBorderLinkedin = document.querySelector(".js-border-linkedin");
+const colorBorderGithub = document.querySelector(".js-border-github");
 
 let data = {
   name: "Nombre Apellido",
@@ -60,4 +73,31 @@ function clickReset() {
   data.github = "";
 }
 
+function colorReset (event){
+  if (event.currentTarget.value !== "color-color-1") {
+    colorName.classList.remove("dark-grey");
+    colorName.classList.remove("dark-red");
+    colorJob.classList.remove("red");
+    colorJob.classList.remove("yellow");
+    colorBorder.classList.remove("red");
+    colorBorder.classList.remove("yellow");
+    colorPhone.classList.remove("dark-red");
+    colorPhone.classList.remove("dark-grey");
+    colorEmail.classList.remove("dark-red");
+    colorEmail.classList.remove("dark-grey");
+    colorLinkedin.classList.remove("dark-red");
+    colorLinkedin.classList.remove("dark-grey");
+    colorGithub.classList.remove("dark-red");
+    colorGithub.classList.remove("dark-grey");
+    colorBorderPhone.classList.remove("orange");
+    colorBorderPhone.classList.remove("grey");
+    colorBorderEmail.classList.remove("orange");
+    colorBorderEmail.classList.remove("grey");
+    colorBorderLinkedin.classList.remove("orange");
+    colorBorderLinkedin.classList.remove("grey");
+    colorBorderGithub.classList.remove("orange");
+    colorBorderGithub.classList.remove("grey");
+  }
+}
 reset.addEventListener("click", clickReset);
+reset.addEventListener ('click',colorReset);
